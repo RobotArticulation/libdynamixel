a42825e2dbfdd7f0488fb0c41d9f6ed2343bcdf1
wigir <wigir@robotarticulation.com>
2020-04-26 20:58:44 +0200

    further work on m30

--- src/dynamixel/servos/m30.hpp
+++ src/dynamixel/servos/m30.hpp
@@ -39,37 +39,56 @@ namespace dynamixel {
                 typedef uint8_t lowest_voltage_limit_t;
                 static const protocol_t::address_t highest_voltage_limit = 13;
                 typedef uint8_t highest_voltage_limit_t;
-                // static const protocol_t::address_t max_torque = 14;
-                // typedef uint16_t max_torque_t;
 
-                static const protocol_t::address_t torque_limit = 15;
+
+                static const protocol_t::address_t torque_limit = 14;
                 typedef uint16_t torque_limit_t;
 
-                // static const protocol_t::address_t alarm_led = 17;
-                // typedef uint8_t alarm_led_t;
+
+                static const protocol_t::address_t angular_velocity_limit = 16;
+                typedef uint16_t angular_velocity_limit_t;
+                static const protocol_t::address_t acceleration_limit = 18;
+                typedef uint16_t acceleration_limit_t;
+                static const protocol_t::address_t home_position_offset = 20;
+                typedef uint32_t home_position_offset_t;
+                static const protocol_t::address_t moving_threshold = 24;
+                typedef uint16_t moving_threshold_t;
+
+                /*
+                 * --- unused, but need to be here to prevent compile errors
+                 */
                 static const protocol_t::address_t status_return_level = 26; // unused
                 typedef uint8_t status_return_level_t;
                 static const protocol_t::address_t alarm_shutdown = 27;  // unused
                 typedef uint8_t alarm_shutdown_t;
+                /*
+                 * --- end of unused bit
+                 */
 
+                // --- ram
                 static const protocol_t::address_t torque_enable = 48;
                 typedef uint8_t torque_enable_t;
-                // static const protocol_t::address_t led = 25;
-                // typedef uint8_t led_t;
-                // static const protocol_t::address_t cw_compliance_margin = 26;
-                // typedef uint8_t cw_compliance_margin_t;
-                // static const protocol_t::address_t ccw_compliance_margin = 27;
-                // typedef uint8_t ccw_compliance_margin_t;
-                // static const protocol_t::address_t cw_compliance_slope = 28;
-                // typedef uint8_t cw_compliance_slope_t;
-                // static const protocol_t::address_t ccw_compliance_slope = 29;
-                // typedef uint8_t ccw_compliance_slope_t;
+
+                static const protocol_t::address_t pKd = 50;
+                typedef uint16_t pKd_t;
+                static const protocol_t::address_t pKi = 52;
+                typedef uint16_t pki_t;
+                static const protocol_t::address_t pKp = 54;
+                typedef uint16_t pkp_t;
+
+
+                static const protocol_t::address_t pKff1 = 56;
+                typedef uint16_t pKff1_t;
+                static const protocol_t::address_t pKff2 = 58;
+                typedef uint16_t pKff2_t;
+
+
                 static const protocol_t::address_t goal_position = 30;
                 typedef uint16_t goal_position_t;
                 static const goal_position_t min_goal_position = 0;
                 static const goal_position_t max_goal_position = 1023;
-                static const uint16_t min_goal_angle_deg = 30;
-                static const uint16_t max_goal_angle_deg = 330;
+                static const uint16_t min_goal_angle_deg = 0;
+                static const uint16_t max_goal_angle_deg = 360;
                 static const protocol_t::address_t moving_speed = 32;
                 typedef uint16_t moving_speed_t;
 
@@ -78,14 +97,10 @@ namespace dynamixel {
                 static constexpr double rpm_per_tick = 0.111;
                 static constexpr bool speed_sign_bit = true;
 
-                // static const protocol_t::address_t torque_limit = 34;
-                // typedef uint16_t torque_limit_t;
                 static const protocol_t::address_t present_position = 36;
                 typedef uint16_t present_position_t;
                 static const protocol_t::address_t present_speed = 38;
                 typedef uint16_t present_speed_t;
-                static const protocol_t::address_t present_load = 40;
-                typedef uint16_t present_load_t;
                 static const protocol_t::address_t present_voltage = 42;
                 typedef uint8_t present_voltage_t;
                 static const protocol_t::address_t present_temperature = 43;
@@ -94,10 +109,6 @@ namespace dynamixel {
                 typedef uint8_t registered_t;
                 static const protocol_t::address_t moving = 46;
                 typedef uint8_t moving_t;
-                static const protocol_t::address_t lock = 47;
-                typedef uint8_t lock_t;
-                static const protocol_t::address_t punch = 48;
-                typedef uint16_t punch_t;
             };
         };
 
@@ -113,17 +124,18 @@ namespace dynamixel {
             READ_WRITE_FIELD(operating_mode);
             READ_WRITE_FIELD(cw_angle_limit);
             READ_WRITE_FIELD(ccw_angle_limit);
-            //READ_WRITE_FIELD(max_torque);
-            // READ_WRITE_FIELD(alarm_led);
-            // READ_WRITE_FIELD(led);
-            // READ_WRITE_FIELD(cw_compliance_margin);
-            // READ_WRITE_FIELD(ccw_compliance_margin);
-            // READ_WRITE_FIELD(cw_compliance_slope);
-            // READ_WRITE_FIELD(ccw_compliance_slope);
+
             READ_WRITE_FIELD(torque_limit);
-            READ_FIELD(present_load);
-            READ_WRITE_FIELD(lock);
-            READ_WRITE_FIELD(punch);
+
+            READ_WRITE_FIELD(angular_velocity_limit);
+            READ_WRITE_FIELD(acceleration_limit);
+            READ_WRITE_FIELD(home_position_offset);
+
+            //READ_WRITE_FIELD(pppppp_Kd);
+            // READ_WRITE_FIELD(pKi);
+            // READ_WRITE_FIELD(pKp);
+
+
         };
     }
 }
